46b4715767612c172e8249320fdda40e8b860ced
ch-ratnaji <ratnaji.it91@gmail.com>
2024-08-30 01:20:52 +0530

    branch1

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,4 +1,16 @@
+import { getUserList } from "../../services/appservice";
+import { useState, useEffect } from "react";
+
 export const UserList = () => {
+  const [userList, setUserList] = useState([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const list = await getUserList();
+      setUserList(list);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <div className="user-list-container">
       <table>
@@ -9,7 +21,16 @@ export const UserList = () => {
             <th>Purchased Amount</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {userList.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.user_name}</td>
+              <td>{user.purchase_history.purchase_amount}</td>
+              <td>click here for more data</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
